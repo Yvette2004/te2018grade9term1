59d84a843969153393a2d4a85b55af2c4ec1e7a8
Myles N <myles.nutifafa@gmail.com>
2018-10-06 15:45:48 -0400

    Today we complete challenge 1

--- data.js
+++ data.js
@@ -1,5 +1,27 @@
 // Example
+const READLINE = require("readline-sync")
 let message = "Hello World!";
 console.log(message);
 
 // Type your code below!
+
+let pets = 1;
+console.log(pets);
+let lives = 5;
+console.log(lives);
+let week =27;
+console.log(week);
+let whoAmI ="Im awesome";
+console.log( whoAmI);
+let favoriteClass ="math";
+console.log(favoriteClass);
+let myName="Yvette is who i am";
+console.log(myName);
+let x =true;
+console.log(x);
+let y =false;
+console.log(y)
+
+let userInput = READLINE.question("Type anything and I'll repeat it back")
+console.log(userInput)
+bard
