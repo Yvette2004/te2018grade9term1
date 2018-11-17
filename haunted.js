3211fc98ec5c07700459895bdb6f59f310ac961a
yvette rodriguez <yvette.rodriguez@csedge.org>
2018-11-17 12:46:18 -0800

    update

--- haunted.js
+++ haunted.js
@@ -17,74 +17,73 @@ console.log(`${player.name} stands in front of a spooky haunted house.`);
 console.log(`${player.name} hears what sounds like screaming coming from inside the house.`);
 let enterHouse = READLINE.question("Will you enter the house? (yes or no or teleport or abandon) ");
 
-if(enterHouse == "y" || enterHouse == "yes") 
-{
+if(enterHouse == "y" || enterHouse == "yes") {
   player.courage++;
   console.log(`${player.name} prepares their ${player.weapon} and opens the door...`);
-  // continue the story
- console.log(`${player.name} sees a figure and starts hitting it. Quickly realizing that nothing was  actually there`);
+  console.log(`${player.name}  sees a figure and starts hitting it. Quickly realizing that nothing was  actually there.`);
  
- let talkback = READLINE.question("Will you continue?")
-  if (talkback == "yes" || talkback == "maybe")
-   {
-    console.log(`${player.name} takes out her ${player.item} and realizes that there was nothing haunted about it. YOU SURVIVED`);}
- 
-    if(talkback =="n" || talkback == "no")
-    {
-      console.log(`${player.name} you see a shadow lurking.you just died `);
+    let talkback = READLINE.question("Are you sure you want to continue?")
+      if (talkback == "yes" || talkback == "maybe"){
+        console.log(`${player.name} takes out her ${player.item} and head up to the second floor.You pass through the door and into one of the rooms.By the window, you see someone dressed in white looking out of it`);}
+    let okay = READLINE .question("Do you approach her?")
+      if (okay == "yes" || okay == "definitely"){
+       console.log(`${player.name} you slowly approach her and when she turns around, there is no face.She    opens her mouth, and a bunch of bugs crawl out.The bugs spreads to you and eat you up.You died a painful and disturbing death.`);
     }
-  }
+      else if (okay == "no" || okay == "never"){
+      console.log(`${player.name} is you quickly run to the opposite direction.But that same person appears in front of you.She smiles, and without realizing you are already dead. `);
+    }
+      else (talkback =="n" || talkback == "no")
+        {
+          console.log(`${player.name} you died.For natural causes...bye. `);
+    
+      }
+    }
   
-  if (enterHouse == "no") 
-  {
-     player.intellect++;
+else if (enterHouse == "no") {
+    player.intellect++;
   console.log(`${player.name} decides not to open the door. However...`);
-  // continue the 
-console.log(`${player.name}  starts walking in the opposite direction, you spot a old lady.She offers to take you to her house.`);
+  console.log(`${player.name}  starts walking in the opposite direction, you spot a old lady.She offers to take you to her house.`);
    
    let answer =  READLINE.question("Will you follow and enter? (yes or no)")
+      if(answer == "y" || answer == "yes") 
+      {
+      player.charm++;
+      console.log(`${player.name} takes out her ${player.killer}, but it's too late you are already dead `);}
 
-  if(answer == "y" || answer == "yes") 
-{
- player.charm++;
- console.log(`${player.name} takes out her ${player.killer}, but it's too late you are already dead `);}
+      else if(answer =="n" || answer=="no")
+      { 
+        player.intellect++;
+        console.log(`${player.name} You survived`);
+    }
+  }
 
- if(answer =="n" || answer=="no")
- { player.intellect++;
-console.log(`${player.name} You survived`);
- 
+else if (enterHouse == "teleport") {
+  console.log(`${player.name} You decide to teleport and now you are heading to Japan 
+  You are now in an arcade game.No one is there and it is very dark.You see a door that is red.`);
 
-    if (enterHouse == "teleport") 
-    {
-    console.log(`${player.name} You decide to teleport and now you are heading to Japan 
-      You are now in an arcade game.No one is there and it is very dark.You see a door that is red.`);
+    let japan =  READLINE.question("Will you follow and enter? (yes or no)");
+      if(japan == "y" || japan == "yes")
+       {
+      player.charm++;
+      console.log(`${player.name}You go outside and  see a bunch of people all walking to their desired destinationos. You go to the police...You are safe`);}
 
-     let japan =  READLINE.question("Will you follow and enter? (yes or no)");
-if(japan == "y" || japan == "yes") {
- player.charm++;
- console.log(`${player.name}
-  You go outside and   see a bunch of people all walking
-  to their desired destinationos. You go to the police...You are safe`);
-
- if(japan == "n" || japan == "no")
- { player.intellect++;
-console.log(`${player.name} You see a dark shadow approaching from halfway accross
- the room.It starts runing towards you .You take out your ${player.help} and stop time.`);
-  
-
-
-      if(enterHouse =="abandon") {
+        else if(japan == "n" || japan == "no")
+        { 
+          player.intellect++;
+        console.log(`${player.name} You see a dark shadow approaching from halfway accross the room.It starts runing towards you .You take out your ${player.help} and stop time.`);
+    }
+  }
+else if(enterHouse =="abandon") {
   player.intellect++;
   console.log(`${player.name} decides to flee.You are a coward, but smart.`);
   console.log(`${player.name} You survived`);
   
 }
-}
-}
 
-}
- }
-}
+
+
+
+
 
 
 
